bca5eb2eec9f3ca4f4582c11c7b0033aa9d7a260
Bernardo SOUSA <bernardodesousa@gmail.com>
2019-01-16 10:59:03 -0200

    Replace hard-coded list of themes by a dynamically generate one.

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import addTabs from './tabs/add'
 import * as Course from './course'
 // import giveDiscountCode from './users/give-discount-code'
-import * as presentation from './presentation/index'
+// import * as presentation from './presentation/index'
 // import * as Debt from './users/debt/index'
 // import enlistUser from './users/enlist'
 // import freeUserSlot from './users/free-user-slot'
@@ -11,10 +11,23 @@ import { giveXP, publishTestimony } from './users'
 
 addTabs()
 
+// build list of themes based on string.material.themes array
+// console.log(string.material.themes);
+
+let themesList = "";
+let i, l;
+l = string.material.themes.length;
+for (i = 0; i < l; i++) {
+    themesList += `<option value="${i}">${string.material.themes[i].adjective}</option>`;
+}
+
+document.getElementById("create-group-theme").innerHTML = themesList;
+document.getElementById("coordinator-create-pack-theme").innerHTML = themesList;
+
 window.Coordinator = {
     Course,
     // giveDiscountCode,
-    presentation,
+    // presentation,
     // Debt,
     // enlistUser,
     // freeUserSlot,
